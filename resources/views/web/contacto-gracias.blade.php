7801ff67371e0bd8f4f2bb7bbce072b02651f513
dariangaribotti <darian.gb03@gmail.com>
2026-03-01 17:14:45 -0300

    Maquetación de la web-ecommerce

---
+++ resources/views/web/contacto-gracias.blade.php
@@ -0,0 +1,8 @@
+@extends("web.plantilla")
+@section("contenido")
+<div class="row">
+      <div class="col-md-6">
+            <h1>Gracias por mandar tu postulación</h1>
+      </div>
+</div>
+@endsection
